bddbb1c0a59b7a48af65247abb7c8c0ad4a15c85
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-16 13:34:21 -0600

    Reset file to be cloned by group

--- DeepDiveConnect/src/main.jsx
+++ DeepDiveConnect/src/main.jsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx"
-import EmployeeSearchResult from './EmployeeSearchResult.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <EmployeeSearchResult/>
-    <EmployeeSearchResult/>
-    <EmployeeSearchResult/>
-    <EmployeeSearchResult/>
   </StrictMode>,
 )
