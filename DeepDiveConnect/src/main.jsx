ecbcc785b411850d2a64fb201c064ccc4d628a9a
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-18 15:14:48 -0600

    Started header, added logo vector

--- DeepDiveConnect/src/main.jsx
+++ DeepDiveConnect/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import FootIcons from "./components/DeepDiveFooter.jsx";
+import Header from "./components/Header.jsx"
 
 createRoot(document.getElementById("root")).render(
   <>
+    <Header />
     <App />
     <FootIcons />
   </>
