2939f38a79601afa8c3b88dd082d0dba3c0fe213
Daniel Andres Arismendi Barrera <daab@Mac.attlocal.net>
2025-01-17 21:01:46 -0600

    create footer

---
+++ DeepDiveConnect/src/components/DeepDiveFooter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function FootIcons() {
+  return (
+    <>
+      <footer>Space for the icons</footer>
+      <img
+        className="avatar"
+        src="/public/images/homeicon.png"
+        alt="Lin Lanying"
+        width={50}
+        height={50}
+      />
+    </>
+  );
+}
+export default FootIcons;
