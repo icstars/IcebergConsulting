0cb0772de9f1fb951a675d4163b829f418f844ee
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-25 13:05:31 -0600

    Created server ports

--- DeepDiveConnect/server.js
+++ DeepDiveConnect/server.js
@@ -1,10 +1,25 @@
 import express from "express";
-import cors from "cors";
 const app = express();
 
 const port = 3000;
-app.use(cors({origin: "http://localhost:5174"}))
 
 app.get('/', (req, res) => {
-    res.send('hola gonorrea');
+    res.send('Home');
 })
+
+app.get('/profile', (req, res) => {
+    res.send('User Profile');
+})
+
+app.get('/contacts', (req, res) => {
+    res.send('Contact Cards');
+})
+
+app.get('/settings', (req, res) => {
+    res.send('Settings');
+})
+
+app.listen(port, () => {
+    console.log('DeepDive listening on port ${port}')
+})
+
