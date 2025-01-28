da32d047cbdd31cf0c417a7c70a52d1f507874f7
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-28 08:22:28 -0600

    Styling header and editing backend ports

--- DeepDiveConnect/server.js
+++ DeepDiveConnect/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 
 const port = 3000;
+app.use(cors({origin: 'http://localhost:5174' }));
 
 app.get('/', (req, res) => {
     res.send('Home');
@@ -20,6 +22,6 @@ app.get('/settings', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('DeepDive listening on port ${port}')
+    console.log(`DeepDive listening on port ${port}`)
 })
 
