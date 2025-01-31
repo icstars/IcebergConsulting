5dcde1e6a785bed5a5c4216d19589c88e55bceca
Daniel Andres Arismendi Barrera <daab@Daniels-MacBook-Pro.local>
2025-01-31 15:39:28 -0600

    correct style

--- DeepDiveConnect/src/App.jsx
+++ DeepDiveConnect/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.module.css";
 import { Navbar } from "./components/Navbar";
 import ChatBot from "./pages/home";
--- DeepDiveConnect/src/components/Navbar.jsx
+++ DeepDiveConnect/src/components/Navbar.jsx
@@ -9,10 +9,7 @@ export const Navbar = () => {
   return (
     <nav>
       <Link to="/" className="title">
-        <img className="logo"
-        src="./public/Images/ddclogosvg.svg"
-        alt ="Logo Deep Dive"
-        />
+        <img className="logo" src="./Images/ddlogo.png" alt="Logo Deep Dive" />
       </Link>
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
@@ -20,14 +17,30 @@ export const Navbar = () => {
         <span></span>
       </div>
       <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink to="/contacts">Contacts</NavLink>
+        <li className="selection">
+          <NavLink to="/contacts">
+            <img className="icon" src="./Images/contacts.svg" alt="resources" />
+            <br></br>
+            <p>Contacts</p>
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/location">Locations</NavLink>
+          <NavLink to="/location">
+            <img className="icon" src="./Images/location.svg" alt="resources" />
+            <br></br>
+            <p>Locations</p>
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/resources">Resources</NavLink>
+          <NavLink to="/resources">
+            <img
+              className="iconbook"
+              src="./Images/resources.svg"
+              alt="resources"
+            />
+            <br></br>
+            <p>Resources</p>
+          </NavLink>
         </li>
       </ul>
     </nav>
